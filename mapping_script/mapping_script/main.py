f5249e3e1b7347af95b8bd6faf084c07e3ba7ea5
nvim <prozskov@gmail.com>
2024-12-13 16:20:37 +0100

    Change order of columns using config

--- mapping_script/mapping_script/main.py
+++ mapping_script/mapping_script/main.py
@@ -32,42 +32,33 @@ def save_patients_mapping_file(path_to_result, mapped_patient_ids):
     with open(full_path_to_result, 'w') as file:
         yaml.dump({"patients": mapped_patient_ids}, file, default_flow_style=False, sort_keys=False)
 
-def replace_data(generated_data, mapped_patient_ids, mapping_config, add_id_column, rename_files, delete_files, path_to_result):
+def replace_data(generated_data, mapped_patient_ids, mapping_config, add_id_column,
+                 column_list, rename_files, delete_files, path_to_result):
     for resource_type in generated_data.keys():
         if resource_type in delete_files:
-            print(f"Skip processing of the {resource_type}")
             continue
 
         modified_df = generated_data[resource_type]
 
         if resource_type in add_id_column:
-            print(f"Add id column for {resource_type}")
             modified_df['id'] = [uuid.uuid4() for _ in range(len(modified_df))]
 
-        print("Replace patient ids if needed...")
         modified_df.replace(mapped_patient_ids, inplace=True)
 
         if resource_type in mapping_config:
             for mapping_config_item in mapping_config[resource_type]:
-                print(f"Rename column from {mapping_config_item['from']} to {mapping_config_item['to']} for {resource_type}")
                 modified_df.rename(columns={mapping_config_item['from']: mapping_config_item['to']}, inplace=True)
 
+        if resource_type in column_list.keys():
+            cols = column_list[resource_type]
+            for col in cols:
+                if col not in modified_df.columns:
+                    modified_df[col] = ''
+            modified_df = modified_df[cols]
         result_filename = f"{rename_files.get(resource_type, resource_type)}.csv"
-        print(f"Save file with name {result_filename}")
 
         output_path = os.path.join(path_to_result, result_filename)
         modified_df.to_csv(output_path, index=False)
-
-def check_config(config_data):
-    problems = []
-
-    mapping_config = config_data.get('mapping', {})
-    mapping_config_keys = mapping_config.keys()
-
-    if len(mapping_config_keys) != len(list(set(mapping_config_keys))):
-        problems.append("Duplicate keys in mapping variable")
-
-    return problems
 
 def main(path_to_original, path_to_config, path_to_result):
     try:
@@ -78,6 +69,7 @@ def main(path_to_original, path_to_config, path_to_result):
         rename_files = config.get('rename_files', {})
         delete_files = config.get('delete_files', [])
         add_id_column = config.get('add_ids_column', [])
+        column_list = config.get('columns_list', {})
 
         if not target_patient_ids:
             print("No target_patient_ids found in the YAML configuration.")
@@ -85,7 +77,9 @@ def main(path_to_original, path_to_config, path_to_result):
         mapped_patient_ids = dict(zip(target_patient_ids, list(data['patients']['Id'])))
 
         save_patients_mapping_file(path_to_result, mapped_patient_ids)
-        replace_data(data, mapped_patient_ids, mapping_config, add_id_column, rename_files, delete_files, path_to_result)
+        replace_data(data, mapped_patient_ids, mapping_config,
+                     add_id_column, column_list, rename_files,
+                     delete_files, path_to_result)
 
     except Exception as e:
         print(f"An error occurred: {e}")
